430b6afe09b5dc09c7d49e20aecb0aff8eb1dbb8
ThomasGiovinazzo02 <42356815+ThomasGiovinazzo02@users.noreply.github.com>
2018-08-13 16:58:48 -0400

    java training
    string name, int age, double score, char group, and boolean online added // TG

--- Controls-BootCamp/src/Heralb.java
+++ Controls-BootCamp/src/Heralb.java
@@ -2,6 +2,11 @@
 public class Heralb {
 	public static void main (String[] argos) {
 		System.out.println("Kaaaachow");
+		String name = "heralb";
+				int age = 7089;
+				double score = 43.78;
+				char group = 'H';
+				boolean online = false;
 	}
 
 }

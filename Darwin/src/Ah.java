0f13900fa5c91c3119dc3ecbac7d3cac37ecd1a2
dkwit <d.k87@hotmail.com>
2018-08-13 17:56:51 -0400

    first day

---
+++ Darwin/src/Ah.java
@@ -0,0 +1,23 @@
+
+public class Ah {
+public static void main(String[] args) {
+	String name = "David";
+	int age = 25;
+	double score = 15.9;
+	char group = 'Z';
+	boolean online = true;
+	int x = 7;
+	if(x < 42) {
+		System.out.println("Hi");
+	if (age <= 0) {
+		System.out.println("Error");
+	}else if(age <= 16) {
+		System.out.println("Too Young");
+	}else if(age < 100) {
+		System.out.println("Welcome");
+	}else
+		System.out.println("Really?");
+	}
+	}
+}
+}

23c4c295075a4392dda5204e09d6e0c988d6d208
ThomasGiovinazzo02 <42356815+ThomasGiovinazzo02@users.noreply.github.com>
2018-08-13 18:00:22 -0400

    java training
    did some stuff i guess //TG

--- Controls-BootCamp/src/Heralb.java
+++ Controls-BootCamp/src/Heralb.java
@@ -1,12 +1,27 @@
+import java.util.Scanner;
 
 public class Heralb {
-	public static void main (String[] argos) {
+	public static void main(String[] argos) {
+		Scanner myVar = new Scanner(System.in);
+		System.out.println(myVar.nextLine());
 		System.out.println("Kaaaachow");
 		String name = "heralb";
-				int age = 7089;
-				double score = 43.78;
-				char group = 'H';
-				boolean online = false;
+		int age = 7089;
+		double score = 43.78;
+		char group = 'H';
+		boolean online = false;
+		int x = 7;
+		int y = 8;
+		if (x < y)
+			System.out.println("damn got yoted");
+		if (age <= 0) {
+			System.out.println("qpples");
+		} else if (age <= 100) {
+			System.out.println("ooooof");
+		} else if (age <= 1000) {
+			System.out.println("no u dude");
+		} else if (age <= 9000) {
+			System.out.println("damn guess ill die");
+		}
 	}
-
 }

6e3303b2258f13059e9c1bee7eb9d1c56f8ee1c1
Miguel Lopez <42356670+Tacotroll@users.noreply.github.com>
2018-08-13 17:57:22 -0400

    Controls Camp Lessons
    Practice Code

--- Controls-BootCamp/src/Quesadillas.java
+++ Controls-BootCamp/src/Quesadillas.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
 
 public class Quesadillas {
 	public static void main(String[] args) {
 		System.out.println("Tacos are Great");
+		String name ="Heralb";
+		int age = 638;
+		double confirmed_kills =9000.5;
+		char group = 'E';
+		boolean online=false;
+		//Scanner myVar = new Scanner(System.in);
+		//System.out.println(myVar.nextLine());
+		
+		int x = 10000000;
+		if(x>=9000) {
+			System.out.println("SLIGHTLY OVER NINE THOUSAAAAAND");}
+		else if(x>=9500) {
+			System.out.println("OVER NINE THOUSAAAAAND");}
+		else if(x>=10000) {
+			System.out.println("WAYYY OVER NINE THOUSAAAAAND");}
+		else if (x<=8999) {
+			System.out.println("NOT EVEN CLOSE TO NINE THOUSAND");
+		}
+		
 		}
 
 }
